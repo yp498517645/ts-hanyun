c195290fba0d886d145de33cb01604629daffba8
Alonced <498517645@qq.com>
2022-02-23 15:45:46 +0800

    初始化

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import './App.css'
 
 function App() {
   return (
-    <div>wwww</div>
+    <div></div>
   );
 }
 
---
+++ src/pages/Login/index.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+import { Form, Input, Button, Checkbox } from "antd";
+
+export default function Login() {
+  return <div>'ww</div>;
+}
